2a126fa8142d1c56385b47d0063d033d428fa3df
jkolb09 <60716970+jkolb09@users.noreply.github.com>
2020-04-01 11:59:19 -0700

    Add files via upload

---
+++ PHP Arrays Project.php
@@ -0,0 +1,9 @@
+<?php
+$a = array(1, 2, 3, 4,5,6,7,8,9,10);
+echo "sum(a) = " . array_sum($a) . "\n";
+ print_r(array_count_values($a));
+ $average= array_sum($a)/count($a);
+echo $average
+    
+    
+?> 
